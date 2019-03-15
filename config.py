67ff87d0f5cb992b1e1b842cd4bae2713674ee35
gaunthan <gaunthanhuang@foxmail.com>
2019-03-15 18:27:31 +0800

    add configuration file

---
+++ config.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+# Configurations to run the program properly
+
+# Account Settings
+#
+# Your email
+email = ''
+# Your password
+pwd = ''
+
+# Basic Output Settings
+#
+# Path to save your notes
+output_path = '../my-hexo-blog/source/_posts'
+# Only output blog note if True
+only_blog = True
+
+# Image Output Settings
+#
+# Save notes' images if True
+localize_image = True
+# Path to save notes' images
+img_path = '../my-hexo-blog/source/images'
+# Path that image links references
+# For instance:
+#   - Original link: ![](https://web.image.com/my_image.png) 
+#   - If set this var: ![](/images/my_image.png) 
+img_link_path = '/images'
+# Force to save images if True, even though already downloaded
+forced_save = False
